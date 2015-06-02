fbbcbbd92fb5d7f61150add077b021c71c522c19
Sam Roberts <sam@strongloop.com>
2015-06-01 21:41:57 -0700

    app: pipe child stdio on restart
    stdio was being piped on first run, and unpiped on stop, but not
    re-piped when re-started.

---
+++ test/test-app-stdio.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var App = require('../').App;
+var assert = require('assert');
+var byline = require('byline');
+var commit = require('./commit');
+var debug = require('debug')('strong-runner:test');
+var tap = require('./tap');
+
+tap.test('stdio for workers', function(t) {
+  var app = commit.app();
+
+  var r = App({
+    start: 'sl-run --cluster=0 --no-profile'
+      + '  --no-timestamp-workers --no-timestamp-supervisor'
+  });
+
+  var stdout = byline(r.stdout);
+
+  t.test('run app', function(tt) {
+    debug('wait for run');
+    r.run(app);
+    tt.plan(1);
+    watchFor(tt, /supervisor starting/);
+  });
+
+  t.test('stop app', function(tt) {
+    debug('wait for stop');
+    tt.plan(2);
+    r.stop('hard', function(err) {
+      debug('stopped: err? %s', err);
+      tt.ifError(err, err || 'stop ok');
+    });
+    watchFor(tt, /supervisor stopped/);
+  });
+
+  t.test('start app', function(tt) {
+    debug('wait for start');
+    tt.plan(2);
+    r.start(function(err) {
+      debug('started: err? %s', err);
+      tt.ifError(err, err || 'start ok');
+    });
+    watchFor(tt, /supervisor starting/);
+  });
+
+  function watchFor(t, rx) {
+    var saw;
+    stdout.on('data', function(line) {
+      if (saw) return;
+      debug('is %s in <%s>', rx, line);
+      if (!saw && rx.test(line)) {
+        saw = true;
+        debug('saw rx %s', rx);
+        t.assert(true, 'saw rx ' + rx);
+      }
+    });
+  }
+
+  this.on('end', function() {
+    r.stop('soft');
+  });
+});
